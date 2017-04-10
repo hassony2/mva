67bfb5410abf75349f4d804f698449586598c624
hassony2 <yanahasson@gmail.com>
2017-04-10 16:08:47 +0200

    Add c3d

---
+++ object-recognition/time-arrow-c3d-net/examples/two-videos/launch-train-accuracy.sh
@@ -0,0 +1,3 @@
+caffe test -model c3d_arrow_test.prototxt -weights=c3d_arrow/c3d_arrow_iter_500.caffemodel  -gpu 0 -iterations 267
+caffe test -model c3d_arrow_test.prototxt -weights=c3d_arrow/c3d_arrow_iter_1000.caffemodel  -gpu 0 -iterations 267
+caffe test -model c3d_arrow_test.prototxt -weights=c3d_arrow/c3d_arrow_iter_1400.caffemodel  -gpu 0 -iterations 267
